7a450c196777e5d551149855971bbea2fc76a070
victordantas1 <victorr.dantas@gmail.com>
2025-04-22 15:34:40 -0400

    Add some tests

--- src/main.py
+++ src/main.py
@@ -96,24 +96,24 @@ def main():
         print("2 - Acessar Conta")
         print("3 - Deletar Conta")
         print("0 - Sair")
-        opcao = input("Escolha uma opcao: ")
+        opcao = int(input("Escolha uma opcao: "))
 
-        if opcao == "1":
-            cria_conta(lista_usuarios, lista_contas)
+        match opcao:
+            case 1:
+                cria_conta(lista_usuarios, lista_contas)
+            case 2:
+                cpf = input("Digite o CPF: ")
+                conta_encontrada = next((c for c in lista_contas if c.usuario.cpf == cpf), None)
+                if conta_encontrada:
+                    print(f"\n Bem-vindo(a), {conta_encontrada.usuario.nome.upper()} !")
+                    print(f" Conta: {conta_encontrada.numero_conta}")
+                    acessar_conta(conta_encontrada)
 
-        if opcao == "2":
-            cpf = input("Digite o CPF: ")
-            conta_encontrada = next((c for c in lista_contas if c.usuario.cpf == cpf), None)
-            if conta_encontrada:
-                print(f"\n Bem-vindo(a), {conta_encontrada.usuario.nome.upper()} !")
-                print(f" Conta: {conta_encontrada.numero_conta}")
-                acessar_conta(conta_encontrada)
-
-            else:
-                print("Conta não encontrada!")
-        if opcao == "0":
-            print("Saindo...")
-            break
+                else:
+                    print("Conta não encontrada!")
+            case 3:
+                print("Saindo...")
+                break
 
 if __name__ == "__main__":
     main()
